7d217cf4d248bf9e982324f8f0a9016f9967828f
Yuri Morini <yuri@yurimorini.it>
2017-06-24 16:24:26 +0200

    license

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A simple Vector Lib to solve Udacity Linear Algebra Refresher Course
+
+For testing: `yarn run test`
--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const Vector = require('./lib/Vector.js');
-const Calc = require('./lib/Calc.js');
-const log =  require('./lib/Log.js').log;
+const Vector = require('./lib/Vector');
+const Calc = require('./lib/Calc');
 
-const v1 = new Vector(100, 200);
-const v2 = new Vector(100, 200);
-const v3 = new Vector(200, 100);
-
-
-log(v1);
-
-log(Calc.diff(v1, v3));
-
-log(Calc.scalar(2, v1));
+module.exports = {
+  Vector,
+  Calc
+};
